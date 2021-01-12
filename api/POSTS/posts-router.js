2cb425d1980fd940d8f97a1d633e1b2e18f41097
c_melchor <cah.melchor@gmail.com>
2021-01-12 13:40:16 -0800

    updated POST code, working on condition that both title and contents are provided

--- api/POSTS/posts-router.js
+++ api/POSTS/posts-router.js
@@ -24,14 +24,30 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
-  Posts.insert(req.body)
-    .then(post => {
-      res.status(201).json(post);
-    })
-    .catch(error => {
-      res.status(500).json({ message: error.message });
+router.post("/", async (req, res) => {
+  const newPost = req.body;
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
     });
+  } else {
+    try {
+      const newlyMadePost = await Posts.insert(newPost);
+      res.status(201).json(newlyMadePost);
+    } catch (error) {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
+    }
+  }
 });
+
+// router.put("/:id", (req, res)=> {
+//     Posts.update()
+//     .then(postUpdate=>{
+//         res.status(202)
+//     })
+//     .catch()
+// })
 
 module.exports = router;
