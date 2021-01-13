6bc05e75e2080038a95ef82aa139933a80030601
c_melchor <cah.melchor@gmail.com>
2021-01-12 16:52:31 -0800

    GET request for comments by id in async now

--- api/COMMENTS/comments-router.js
+++ api/COMMENTS/comments-router.js
@@ -2,21 +2,21 @@ const express = require("express");
 const Comments = require("../db-helpers");
 const router = express.Router();
 
-router.get("/:id/comments", (req, res) => {
+router.get("/:id/comments", async (req, res) => {
   const id = req.params.id;
-  Comments.findPostComments(id)
-    .then(post => {
-      if (post && post.length > 0) {
-        res.status(200).json(post);
-      } else {
-        res
-          .status(404)
-          .json({ message: "The post with the specified ID does not exist." });
-      }
-    })
-    .catch(() => {
-      res.status(500).json({ errorMessage: "id not found" });
-    });
+  const commentsFound = await Comments.findCommentById(id);
+
+  if (commentsFound && commentsFound.length > 0) {
+    try {
+      res.status(200).json(commentsFound);
+    } catch (error) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } else {
+    res.status(500).json({ errorMessage: "id not found" });
+  }
 });
 
 module.exports = router;
