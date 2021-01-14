ddea5149ca7f497c2b21184b2317411938168c0d
c_melchor <cah.melchor@gmail.com>
2021-01-14 11:38:12 -0800

    added get to server.js

--- api/server.js
+++ api/server.js
@@ -7,4 +7,12 @@ server.use(express.json());
 server.use("/api/posts", postsRouter);
 server.use("/api/posts", commentsRouter);
 
+server.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({
+      name: "Christina's Deployment",
+      enviornment: process.env.NODE_ENV
+    });
+});
 module.exports = server;
